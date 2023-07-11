a1c8bfc4f7d7d97701f509e595f8425068ee7ccc
Bugart-Lan <lanbugart@gmail.com>
2023-07-11 11:49:27 +0800

    update

--- src/main/java/com/demo/obfuscationdemo/ObfuscationDemoViewController.java
+++ src/main/java/com/demo/obfuscationdemo/ObfuscationDemoViewController.java
@@ -1,11 +1,14 @@
 package com.demo.obfuscationdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ObfuscationDemoViewController {
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/greeting")
     public String greetingForm(Model model) {
@@ -17,5 +20,19 @@ public class ObfuscationDemoViewController {
     public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
         model.addAttribute("greeting", greeting);
         return "result";
+    }
+    @PostMapping("/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+        User user = new User();
+//        user.setId(20);
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);
+        return "Saved";
+    }
+
+    @GetMapping("/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
--- src/main/java/com/demo/obfuscationdemo/User.java
+++ src/main/java/com/demo/obfuscationdemo/User.java
@@ -1,17 +1,19 @@
 package com.demo.obfuscationdemo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-@Entity
+import jakarta.persistence.*;
+
+@Entity(name="test2")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
     private String name;
+    private String email;
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
